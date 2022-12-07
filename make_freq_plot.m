8cd8e25e4fb35da325fa8faf9b50c7149480cb3f
An Grocki <angrocki02@gmail.com>
2022-12-07 18:09:45 -0500

    Make function

--- make_freq_plot.m
+++ make_freq_plot.m
@@ -1,4 +1,4 @@
-function make_freq_plot(x, Fs)
+function [f, m] = make_freq_plot(x, Fs)
 
 % make a frequency domain plot with frequency labels
 %
@@ -8,8 +8,14 @@ function make_freq_plot(x, Fs)
 if(mod(length(x),2) ~=0)
     x = x(1:end-1);
 end
+freq = linspace(-Fs/2, Fs/2-Fs/length(x),length(x));
+mag = 1/length(x)*fftshift(abs(fft(x)));
+[m, index] = max(mag);
+f = abs(freq(index));
 
 plot(linspace(-Fs/2, Fs/2-Fs/length(x),length(x)), 1/length(x)*fftshift(abs(fft(x))))
 
 ylabel('Magnitude of signal');
 xlabel('Frequency (Hz)');
+subtitle("Max Magnitude = " + m +"at frequency " + f +"Hz")
+end 
